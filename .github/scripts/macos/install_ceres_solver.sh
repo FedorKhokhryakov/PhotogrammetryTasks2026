000dc0c4afe74756ec1c4f3fc001ff627b0e4f0b
Boris <simiyutin.boris@yandex.ru>
2026-03-31 13:37:20 +0300

    Force create build directory for Ceres Solver (macos)
    Remove existing build directory before creating a new one.

--- .github/scripts/macos/install_ceres_solver.sh
+++ .github/scripts/macos/install_ceres_solver.sh
@@ -6,7 +6,8 @@ brew install eigen glog gflags suite-sparse
 wget https://github.com/ceres-solver/ceres-solver/archive/2.2.0.zip
 unzip 2.2.0.zip
 cd ceres-solver-2.2.0
-mkdir -p build && cd build
+rm -rf build
+mkdir build
 cmake -DCMAKE_INSTALL_PREFIX=/opt/ceres-solver220 -DUSE_CUDA=OFF ..
 make -j$(sysctl -n hw.ncpu)
 sudo make install
